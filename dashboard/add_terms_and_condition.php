afe0c8035ef102a51616112ccc543b9722335fd0
roshnirana-2 <roshnirana0369@gmail.com>
2024-06-28 16:35:28 +0530

    terms and condition with CRUD operation

---
+++ dashboard/add_terms_and_condition.php
@@ -0,0 +1,149 @@
+<?php
+    include "header.php" ; 
+
+if (isset($_COOKIE['edit_id'])) { $mode='edit' ; $editId=$_COOKIE['edit_id'];
+    $stmt=$obj->con1->prepare("SELECT * FROM `terms_and_conditions` WHERE id=?");
+    $stmt->bind_param('i', $editId);
+    $stmt->execute();
+    $data = $stmt->get_result()->fetch_assoc();
+    $stmt->close();
+    }
+
+    if (isset($_COOKIE['view_id'])) {
+    $mode = 'view';
+    $viewId = $_COOKIE['view_id'];
+    $stmt = $obj->con1->prepare("SELECT * FROM `terms_and_conditions` WHERE id=?");
+    $stmt->bind_param('i', $viewId);
+    $stmt->execute();
+    $data = $stmt->get_result()->fetch_assoc();
+    $stmt->close();
+    }
+
+    if (isset($_REQUEST["save"])) {
+        $details = $_REQUEST["details"];
+        $date = $_REQUEST["date"];
+        $time = $_REQUEST["time"];
+        $date_time = $date . ' ' . $time;
+        $operation = "Added";
+        
+        try {
+            // echo"INSERT INTO `terms_and_conditions`(`details`, `date_time`,`operation`) VALUES (".$details.", ".$date_time.",".$operation.")";
+            $stmt = $obj->con1->prepare("INSERT INTO `terms_and_conditions`(`details`, `date_time`,`operation`) VALUES (?,?,?)");
+            $stmt->bind_param("sss", $details, $date_time,$operation);
+            $Resp = $stmt->execute();
+            if (!$Resp) {
+                throw new Exception(
+                    "Problem in adding! " . strtok($obj->con1->error, "(")
+                );
+            }
+            $stmt->close();
+        } catch (\Exception $e) {
+            setcookie("sql_error", urlencode($e->getMessage()), time() + 3600, "/");
+        }
+        
+        if ($Resp) {
+            
+                setcookie("msg", "data", time() + 3600, "/");
+                header("location:terms_and_condition.php");
+            } else {
+                    setcookie("msg", "fail", time() + 3600, "/");
+                    header("location:terms_and_condition.php");
+                }
+            }
+            
+            if (isset($_REQUEST["update"])) {
+                $e_id = $_COOKIE['edit_id'];
+                $details = $_REQUEST["details"];
+                $date = $_REQUEST["date"];
+                $time = $_REQUEST["time"];
+                $date_time = $date . ' ' . $time;
+                $operation = "Updated";
+                        
+                try {
+                    $stmt = $obj->con1->prepare("UPDATE `terms_and_conditions` SET `details`=?, `date_time`=?,`operation`=? WHERE `id`=?");
+                    $stmt->bind_param("sssi",  $details, $date_time,$operation,$e_id);
+                    $Resp = $stmt->execute();
+                    if (!$Resp) {
+                        throw new Exception(
+                            "Problem in updating! " . strtok($obj->con1->error, "(")
+                        );
+                    }
+                    $stmt->close();
+                } catch (\Exception $e) {
+                    setcookie("sql_error", urlencode($e->getMessage()), time() + 3600, "/");
+                }
+                
+                if ($Resp) {
+                    setcookie("edit_id", "", time() - 3600, "/");
+                    setcookie("msg", "update", time() + 3600, "/");
+                    header("location:terms_and_condition.php");
+                } else {
+                    setcookie("msg", "fail", time() + 3600, "/");
+                    header("location:terms_and_condition.php");
+                }
+            }
+    ?>
+<div class="pagetitle">
+    <h1>Terms And Conditions</h1>
+    <nav>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+            <li class="breadcrumb-item">Terms And Conditions</li>
+            <li class="breadcrumb-item active">
+                <?php echo (isset($mode)) ? (($mode == 'view') ? 'View' : 'Edit') : 'Add' ?> Info</li>
+        </ol>
+    </nav>
+</div>
+<!-- End Page Title -->
+<section class="section">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-body">
+
+                    <!-- Multi Columns Form -->
+                    <form class="row g-3 pt-3" method="post">
+
+                        <div class="col-md-12">
+                            <label for="details" class="col-sm-2 col-form-label">Details</label>
+                            <textarea class="tinymce-editor" name="details" id="details"
+                                <?php echo isset($mode) && $mode == 'view' ? 'disabled' : '' ?>><?php echo (isset($mode)) ? $data['details'] : '' ?></textarea>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="inputDate" class="col-sm-2 col-form-label">Date</label>
+                            <input type="date" class="form-control" id="date" name="date"
+                                value="<?php echo (isset($mode)) ? date('Y-m-d', strtotime($data['date_time'])) : '' ?>"
+                                <?php echo isset($mode) && $mode == 'view' ? 'readonly' : '' ?>>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="inputTime" class="col-sm-2 col-form-label">Time</label>
+                            <input type="time" class="form-control" id="time" name="time"
+                                value="<?php echo (isset($mode)) ? date('H:i', strtotime($data['date_time'])) : '' ?>"
+                                <?php echo isset($mode) && $mode == 'view' ? 'readonly' : '' ?>>
+                        </div>
+
+                        <div class="text-left mt-4">
+                            <button type="submit"
+                                name="<?php echo isset($mode) && $mode == 'edit' ? 'update' : 'save' ?>" id="save"
+                                class="btn btn-success <?php echo isset($mode) && $mode == 'view' ? 'd-none' : '' ?>"><?php echo isset($mode) && $mode == 'edit' ? 'Update' : 'Save' ?>
+                            </button>
+                            <button type="button" class="btn btn-danger" onclick="window.location='terms_and_condition.php'">
+                                Close</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<script>
+function go_back() {
+    eraseCookie("edit_id");
+    eraseCookie("view_id");
+    window.location = "terms_and_condition.php";
+}
+</script>
+<?php
+        include "footer.php";
+        ?>
---
+++ dashboard/terms_and_condition.php
@@ -0,0 +1,157 @@
+<?php 
+ include "header.php";
+ include "alert.php";
+
+ if(isset($_REQUEST["btndelete"]))
+{
+  $id = $_REQUEST['delete_id'];
+  try
+  {
+    $stmt_del = $obj->con1->prepare("DELETE FROM `terms_and_conditions` WHERE id = ?");
+    $stmt_del->bind_param("i",$id);
+    $Resp=$stmt_del->execute();
+    if(!$Resp)
+    {
+      throw new Exception("Problem in deleting! ". strtok($obj->con1-> error,  '('));
+    }
+    $stmt_del->close();
+  }
+  catch(\Exception  $e) {
+    setcookie("sql_error", urlencode($e->getMessage()),time()+3600,"/");
+  }
+
+  if($Resp)
+  {
+    setcookie("msg", "data_del",time()+3600,"/");
+    header("location:terms_and_condition.php");
+  }
+  else
+  {
+   setcookie("msg", "fail",time()+3600,"/");
+   header("location:terms_and_condition.php");
+ }
+}
+?>
+<script type="text/javascript">
+function add_data() {
+    eraseCookie("edit_id");
+    eraseCookie("view_id");
+    window.location = "add_terms_and_condition.php";
+}
+
+function editdata(id) {
+    eraseCookie("view_id");
+    createCookie("edit_id", id, 1);
+    window.location = "add_terms_and_condition.php";
+}
+
+function viewdata(id) {
+    eraseCookie("edit_id");
+    createCookie("view_id", id, 1);
+    window.location = "add_terms_and_condition.php";
+}
+
+function deletedata(id) {
+    $('#deleteModal').modal('toggle');
+    $('#delete_id').val(id);
+}
+</script>
+
+<!-- Basic Modal -->
+<div class="modal fade" id="deleteModal" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Confirm Deletion</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form method="post" action="terms_and_condition.php">
+                <input type="hidden" name="delete_id" id="delete_id">
+                <div class="modal-body">
+                    Are you sure you want to delete this record?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary" name="btndelete" id="btndelete">Delete</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- End Basic Modal-->
+
+<div class="pagetitle">
+    <h1>Terms And Conditions</h1>
+    <nav>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+            <li class="breadcrumb-item">Terms And Conditions</li>
+            <li class="breadcrumb-item active">Data</li>
+        </ol>
+    </nav>
+</div><!-- End Page Title -->
+
+<section class="section">
+    <div class="row">
+        <div class="col-lg-12">
+
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-title">
+                        <a href="javascript:add_data();"><button type="button" class="btn btn-success"><i
+                                    class="bi bi-plus me-1"></i> Add</button></a>
+                    </div>
+                    <table class="table datatable">
+                        <thead>
+                            <tr>
+                                <th scope="col">Sr no.</th>
+                                <th scope="col">Details</th>
+                                <th scope="col">Date Time</th>
+                                <th scope="col">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $stmt = $obj->con1->prepare("SELECT * FROM `terms_and_conditions` ORDER BY `id` DESC");
+                            $stmt->execute();
+                            $Resp = $stmt->get_result();
+                            $i = 1;
+                            while ($row = mysqli_fetch_array($Resp)) { ?>
+                            <tr>
+
+                                <th scope="row"><?php echo $i; ?></th>
+
+                                <td><?php echo $row["details"] ?></td>
+
+                                <td>
+                                    <?php
+                                     $dateTime = strtotime($row["date_time"]);
+                                     echo date("d-m-Y h:i A", $dateTime);
+                                     ?>
+                                </td>
+
+
+                                <td>
+                                    <a href="javascript:viewdata('<?php echo $row["id"] ?>');"><i
+                                            class="bx bx-show-alt bx-sm me-2"></i></a>
+                                    <a href="javascript:editdata('<?php echo $row["id"] ?>');"><i
+                                            class="bx bx-edit-alt bx-sm text-success me-2"></i></a>
+                                    <a href="javascript:deletedata('<?php echo $row["id"] ?>');"><i
+                                            class="bx bx-trash bx-sm text-danger"></i></a>
+                                </td>
+                            </tr>
+                            <?php $i++;
+                                                  }
+                                             ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php
+include "footer.php";
+?>
