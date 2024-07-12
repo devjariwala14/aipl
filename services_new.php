c57d4ba52f731899fd6437a4928bace1a5595c9a
roshnirana-2 <roshnirana0369@gmail.com>
2024-07-12 16:42:57 +0530

    create about us and services

---
+++ services_new.php
@@ -0,0 +1,76 @@
+<?php
+    include "header.php";
+?>
+<main id="main">
+
+                    <!-- Home Section -->
+                    <section class="page-section pt-90 pb-90 pb-xs-40 bg-dark-alpha-50 parallax-5 light-content" style="background-image: url(images/demo-modern/section-bg-4.jpg)" id="home">
+                    <div class="container position-relative">
+                        
+                        <div class="row">
+                            <div class="col-10 col-sm-9">
+                                <h1 class="hs-title-5 font-alt overflow-hidden mb-0">
+                                    <span class="d-block text-end wow fadeRotateIn">
+                                        Services
+                                    </span>
+                                </h1>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    
+                </section>
+                <!-- End Home Section -->  
+    <!-- Services Section -->
+    <section class="page-section bg-light-1 dark-content" id="services">
+        <div class="container">
+
+            <div class="row mb-100 mb-md-60">
+
+                <div class="col-lg-8 mb-md-50">
+                    <div class="section-descr-extralarge wow linesAnimIn" data-wow-offset="0" data-splitting="lines">
+                        <h2 class="section-title-inline-1">Our Values</h2>
+                        As an award-winning group of passionate professionals, we excel in the realms of branding,
+                        design, and engineering. Our team is driven by a shared mission to make work meaningful,
+                        striving to infuse each project with purpose, creativity, and innovation.
+                    </div>
+                </div>
+
+                <div class="col-lg-4 wow fadeInUp" data-offset="0">
+                    <img src="images/services/1.png" alt="Image Description" />
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <!-- End Services Section -->
+    <section class="page-section bg-light-1 dark-content" id="services">
+        <div class="container">
+
+            <div class="row mb-100 mb-md-60">
+
+            <div class="col-lg-4 wow fadeInUp" data-offset="0">
+                    <img src="images/services/1.png" alt="Image Description" />
+                </div>
+
+                <div class="col-lg-8 mb-md-50">
+                    <div class="section-descr-extralarge wow linesAnimIn" data-wow-offset="0" data-splitting="lines">
+                        <h2 class="section-title-inline-1">Our Values</h2>
+                        As an award-winning group of passionate professionals, we excel in the realms of branding,
+                        design, and engineering. Our team is driven by a shared mission to make work meaningful,
+                        striving to infuse each project with purpose, creativity, and innovation.
+                    </div>
+                </div>
+
+                
+
+            </div>
+        </div>
+    </section>
+</main>
+
+<!-- Footer -->
+<?php
+include "footer.php";
+?>
